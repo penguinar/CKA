049e32a3af70ed4ade8fadc9139b6f54ee0a9523
PenguinAR <71999356+penguinar@users.noreply.github.com>
2023-04-02 23:48:13 -0300

    Update reqs.sh

--- reqs.sh
+++ reqs.sh
@@ -8,11 +8,8 @@ swapoff -a
 
 # SET TEMP + PERSISTENT ROUTE FOR K8S TO OVERRIDE VAGRANT DEFAULTS #
 if [ "$HOSTNAME" != control.example.com ]; then
-    echo "ip route add 10.96.0.0/16 dev eth1" >> /etc/rc.local && chmod +x /etc/rc.local && systemctl enab>
+    echo "ip route add 10.96.0.0/16 dev eth1" >> /etc/rc.local && chmod +x /etc/rc.local && systemctl enable rc-local
     ip route add 10.96.0.0/16 dev eth1
 else
     printf '%s\n' "Master node, do nothing"
 fi
-
-
-
