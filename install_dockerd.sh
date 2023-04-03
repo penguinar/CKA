5eb6c2ad6a8a452d9530e25c43a5ecb8acab0a0c
PenguinAR <71999356+penguinar@users.noreply.github.com>
2023-04-02 23:08:56 -0300

    Update install_dockerd.sh

--- install_dockerd.sh
+++ install_dockerd.sh
@@ -83,8 +83,8 @@ echo 'export PATH=$PATH:/usr/local/go/bin' >> /vagrant/.bash_profile
 
 source /root/.bash_profile
 
-wget https://dl.google.com/go/go1.16.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.16.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.20.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
 
 git clone https://github.com/Mirantis/cri-dockerd.git
 cd cri-dockerd/ && mkdir bin && go build -o ../bin/cri-dockerd
