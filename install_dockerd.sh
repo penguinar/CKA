9acad6e50d92dfa8f7b08b447cd431ceead64847
PenguinAR <71999356+penguinar@users.noreply.github.com>
2023-04-02 23:19:01 -0300

    Update install_dockerd.sh

--- install_dockerd.sh
+++ install_dockerd.sh
@@ -89,10 +89,8 @@ tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
 git clone https://github.com/Mirantis/cri-dockerd.git
 cd cri-dockerd/ && mkdir bin && go build -o ../bin/cri-dockerd
 
-cd ..
-
 install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
-cp -a packaging/systemd/* /etc/systemd/system
+sudo cp -a packaging/systemd/* /etc/systemd/system
 sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
 systemctl daemon-reload
 systemctl enable cri-docker.service
